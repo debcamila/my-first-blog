f60e9de7f0c0db61688d795faaff880b3a0ee9bc
debcamila <camiladeborah72@gmail.com>
2020-03-18 12:34:29 -0300

    create API to DG blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
@@ -7,5 +8,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+
+    path('api/posts/', views.PostList.as_view(), name='post_list'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,12 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
+from rest_framework import generics
+from .serializers import PostSerializer
 
+class PostList (generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
